de035e0b60b2882b9f77bd87c292dbc2426b8f22
unknown <anthonylawson0917@gmail.com>
2024-06-03 05:18:41 +1000

    Changed

--- src/container/Footer.tsx
+++ src/container/Footer.tsx
@@ -13,7 +13,7 @@ const Footer: React.FC<{}> = () => {
             <div className={style.footer}>
                 <div className={style.flex_container}>      
                     <div><a href="https://t.me/BasedBeanie"><img src="./assets/image/twitter.png" alt="telegram"/></a></div>
-                    <div><a href="https://t.me/BasedBeanie"><img src="./assets/image/telegram.png" alt="twitters"/></a></div>
+                    <div><a href="https://x.com/basebeanie?t=2hr9F5pVCVzAO-9p1CMonQ&s=09"><img src="./assets/image/telegram.png" alt="twitters"/></a></div>
                     <div><h1 className={style.footertitle}>BASEDBEANIE</h1></div>
                     <div><p style={{textAlign:'center'}} className={style.comment}>$BEANIE is a meme coin with no intrinsic value or expectation of financial return. There is no formal team or roadmap. <br/>
                         The coin is completely useless and for entertainment purposes only © 2024 by $BEANIE. All rights reserved!</p></div>
