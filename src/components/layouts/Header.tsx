04dfab5280666e310714d06607e4dae572ea7462
unknown <anthonylawson0917@gmail.com>
2024-06-03 01:43:31 +1000

    Changed

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,23 +2,36 @@ import React from 'react';
 import style from "../../styles/components/layouts/NavBar.module.scss";
 
 const Header: React.FC<{}> = () => {
+
   return (
       <div className={style.headercontent}>
         <div className={style.flex_container}>
             <div className={style.flex_left}>
               <h1 className={style.beanie}>$BEANIE</h1>
             </div>
-            <div>
-              <ul className={style.nav}>
-                <li className={style.item}><a className={style.linktext} href="#About">about</a></li>
-                <li className={style.item}><a className={style.linktext} href="#TheJury">the jury</a></li>
-                <li className={style.item}><a className={style.linktext} href="#Tokenomics">tokenomics</a></li>
-                <li className={style.item}><a className={style.linktext} href="#Drop">game</a></li>
-              </ul>
+          
+            <div className={style.flex_middle}>   
+                <div className={style.flex_rightf}>
+                  <a className={style.buybtn} href="https://app.uniswap.org/"><i className="fa fa-shopping-cart"/></a>    
+                  &nbsp;&nbsp;
+                </div>       
+                <div className={style.flex_main}>
+                  <input className={style.menu_toggle} id ="menu-toggle" type="checkbox" />
+                  <label className={style.menu_button_container} htmlFor="menu-toggle">
+                    <div className={style.menu_button}></div>
+                  </label>              
+                  <ul className={style.nav}>
+                    <li className={style.item}><a className={style.linktext} href="#About">about</a></li>
+                    <li className={style.item}><a className={style.linktext} href="#TheJury">the jury</a></li>
+                    <li className={style.item}><a className={style.linktext} href="#Tokenomics">tokenomics</a></li>
+                    <li className={style.item}><a className={style.linktext} href="#Drop">game</a></li>
+                  </ul>           
+                </div>                
             </div>
             <div className={style.flex_right}>
               <a className={style.buybtn} href="https://app.uniswap.org/">Buy $BEAN</a>
             </div>
+
           </div>
         </div> 
   )
