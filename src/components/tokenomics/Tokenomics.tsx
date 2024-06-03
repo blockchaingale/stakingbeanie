846bf9a6420e94705a95380ed608d65e4513b6ca
unknown <anthonylawson0917@gmail.com>
2024-06-03 18:06:40 +1000

    Changed

--- src/components/tokenomics/Tokenomics.tsx
+++ src/components/tokenomics/Tokenomics.tsx
@@ -26,13 +26,13 @@ const Tokenomics: React.FC<{}> = () => {
     return (
         <div className={style.tokenomics}>
             <div className={style.flex_container}>
-                <div><h1>TOKENOMICS</h1></div>
-                <div><img src="./assets/image/Beanie3.png" alt="" className={style.JuryImg}/></div>
-                <div><h1 className={style.burned}>LP: BURNED 🔥</h1></div>
+                <div className={style.others}><h1>TOKENOMICS</h1></div>
+                <div className={style.others}><img src="./assets/image/Beanie3.png" alt="" className={style.JuryImg}/></div>
+                <div className={style.others}><h1 className={style.burned}>LP: BURNED 🔥</h1></div>
                 {/* <div><h2 className={style.address}>{TokenAddress}</h2></div> */}
                 <div className={style.swapinfo}>
                     <div className={style.swaps}><h2>BUY TAX<br/>{'0%'}</h2></div>
-                    <div className={style.swaps}><h2>SUPPLY {tokenAmount}</h2></div>
+                    <div className={style.swaps}><h2>SUPPLY<br/>{tokenAmount}</h2></div>
                     <div className={style.swaps}><h2>SELL TAX<br/>{"0%"}</h2></div>
                 </div>
             </div>            
